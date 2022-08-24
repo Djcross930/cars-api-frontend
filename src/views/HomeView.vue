72a2138d524ae2ae553df82331f967f23089f401
Dan Cross <dancross96@gmail.com>
2022-08-23 22:46:49 -0500

    added index action to frontend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,38 @@
+<script>
+import axios from "axios"
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      cars: []
+    };
+  },
+  created: function () {
+    this.indexCars()
+  },
+  methods: {
+    indexCars: function () {
+      axios.get("/cars").then((response) => {
+        console.log(response.data)
+        this.cars = response.data
+      })
+    }
+  },
+};
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div v-for="car in cars">
+      <p>{{ car.make }}</p>
+      <p> {{ car.year }} </p>
+      <p> {{ car.color }} </p>
+      <br />
+      <br />
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<style>
+</style>
