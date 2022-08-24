642d695b618bda42f9fe07e0d70e076decb96ba7
Dan Cross <dancross96@gmail.com>
2022-08-23 23:04:04 -0500

    added show action to in modal

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,8 @@ export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
-      cars: []
+      cars: [],
+      currentCar: {}
     };
   },
   created: function () {
@@ -16,6 +17,11 @@ export default {
         console.log(response.data)
         this.cars = response.data
       })
+    },
+    showCar: function (theCar) {
+      console.log(theCar)
+      this.currentCar = theCar
+      document.querySelector('#car-details').showModal()
     }
   },
 };
@@ -28,9 +34,17 @@ export default {
       <p>{{ car.make }}</p>
       <p> {{ car.year }} </p>
       <p> {{ car.color }} </p>
+      <button v-on:click="showCar(car)">More Info</button>
       <br />
       <br />
     </div>
+    <dialog id="car-details">
+      <form method="dialog">
+        <p><b>make:</b> {{ currentCar.make }}</p>
+        <p><b>year:</b> {{ currentCar.year }}</p>
+        <p><b>color:</b> {{ currentCar.color }}</p>
+      </form>
+    </dialog>
   </div>
 </template>
 
